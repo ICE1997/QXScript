6613d13af964419cc5e334429e9323ca4810a999
Kiza <592092078@qq.com>
2022-05-10 10:47:38 +0800

    feat: fuck you 48h

--- fourty-eight.js
+++ fourty-eight.js
@@ -1,9 +1,22 @@
 let body = JSON.parse($response.body);
 let pcrInfo = body.data.pcrInfo;
-pcrInfo.pcrTime = "05-09 15:33";
+pcrInfo.pcrTime = pcrTime();
 pcrInfo.pcrStatus = "1";
 pcrInfo.newPcrStatus = "1";
 pcrInfo.pcrResult = "1";
 body.data.travelInfo = "1";
 body.data.pcrInfo = pcrInfo;
-$done({body: JSON.stringify(body)});
+$done({ body: JSON.stringify(body) });
+
+function pcrTime() {
+  let date = new Date();
+  var yesterday = new Date(date.getTime());
+  yesterday.setDate(date.getDate() - 1);
+
+  let month = yesterday.getMonth + 1;
+  if (month.length < 2) month = "0" + month;
+  let day = yesterday.getDate();
+  if (day.length < 2) day = "0" + day;
+
+  return month + "-" + day + " 15:33";
+}
